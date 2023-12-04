b33cb265e3fd48d5ec7d35a68e815ab216aa717a
yuweizzz <yuwei764969238@gmail.com>
2023-12-04 15:05:40 +0800

    gitea kustomize yamls

--- init.sh
+++ init.sh
@@ -9,3 +9,11 @@ kubectl kustomize registry/base | kubectl apply -f -
 
 # modify
 # kubectl kustomize registry/base | kubectl apply -f - --prune -l app="registry"
+
+kubectl kustomize gitea/base | kubectl apply -f -
+
+# delete
+# kubectl kustomize gitea/base | kubectl delete -f -
+
+# modify
+# kubectl kustomize gitea/base | kubectl apply -f - --prune -l app="gitea"
